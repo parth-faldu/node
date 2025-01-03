70e992fa229bb328c95bb5a073f6a3d8c86a126b
parth-faldu <parth123faldu@gmail.com>
2025-01-03 16:34:50 +0530

    use environment variable for server port configuration

--- URL_Shortner/app.js
+++ URL_Shortner/app.js
@@ -3,7 +3,7 @@ import { createServer } from 'http';
 import path from 'path';
 import crypto from 'crypto';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const DATA_FILE = path.join("data", "links.json");
 
 const serveFile = async (res, filePath, contentType) => {
